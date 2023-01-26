c39f09dd179307d53354b91b3c841fb16622a3b4
MartinSlash0 <121923440+MartinSlash0@users.noreply.github.com>
2023-01-26 23:19:01 +0200

    Update Regex Password Validation.js

--- Regex Password Validation.js
+++ Regex Password Validation.js
@@ -1,2 +1,12 @@
+/*
+You need to write regex that will validate a password to make sure it meets the following criteria:
+
+    At least six characters long
+    contains a lowercase letter
+    contains an uppercase letter
+    contains a digit
+    only contains alphanumeric characters (note that '_' is not alphanumeric)
+*/
+
 // assign your RegExp to REGEXP
 const REGEXP = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{6,}$/;
