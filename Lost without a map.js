17a43bfc0fad2e937110b5d3d1ee6afb0fb2cc6b
MartinSlash0 <121923440+MartinSlash0@users.noreply.github.com>
2023-02-03 21:08:19 +0200

    Create Lost without a map.js

---
+++ Lost without a map.js
@@ -0,0 +1,14 @@
+/*
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+[1, 2, 3] --> [2, 4, 6]
+*/
+
+function maps(x){
+  let arr = []
+  for(let i=0; i<x.length; i++){
+    arr.push(x[i] * 2)
+  }
+  return arr
+}
