04722c8a15bd4b204841750eaac2845a2d711e99
MartinSlash0 <121923440+MartinSlash0@users.noreply.github.com>
2023-02-22 01:43:24 +0200

    Update Persistent Bugger.js

--- Persistent Bugger.js
+++ Persistent Bugger.js
@@ -1,5 +1,6 @@
 /*
-Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence,
+which is the number of times you must multiply the digits in num until you reach a single digit.
 
 For example (Input --> Output):
 
